f8835e0b206d5523ce65611aced4da3469428e78
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-08-25 00:21:31 +1000

    Add error message

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,5 +1,45 @@
+import { useForm } from "react-hook-form";
+
 function CreatePost() {
-    return <div> CreatePost Page </div>;
+
+    const { register, handleSubmit, formState: { errors },} = useForm ();
+        function onSubmit() {
+        console.log("Form data");
+    }
+    return (
+    <div>
+        <h3> Create a New Post </h3>
+        <form onSubmit={onSubmit}>
+            <div>
+            <label style={{marginRight: 10 }}> Title </label>
+            <input type="text" placeholder="Post title..." {...register("title", {
+                required: "Title is required",
+                minLength: {
+                    value: 3,
+                    message: "Title must be at least 3 characters"
+                }
+            })}/>
+
+            {errors.title && <p style={{ color: "red" }}> {errors.title.message}</p>}
+            </div>
+
+            <div style={{ marginTop: 10 }}>
+            <label style={{marginRight: 10 }}> Content </label>
+            <input type="text" placeholder="Post content..." {...register("content", {
+                required: "Content is required",
+                minLength: {
+                    value: 10,
+                    message: "Title must be at least 10 characters"
+                }
+            })} />
+             {errors.content && <p style={{ color: "red" }}> {errors.content.message}</p>}
+            </div>
+
+            <button type="submit" style={{ marginTop: 10 }}> Create Post</button>
+        </form>
+    
+    </div>
+    );
 }
 
 export default CreatePost;

404ad4b49ac5f58a356bb4ce46cc53d94e03b344
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-08-29 23:47:59 +1000

    Add onClick handler to navigate to edit post page on Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,13 @@
+import { useNavigate } from "react-router-dom"
+
+
 function Post(props) {
+    const navigate = useNavigate();
+    
 return (
-    <div style={{padding: 20, margin:10, width: 300, border: "2px solid black"}}>
+    <div style={{padding: 20, margin:10, width: 300, border: "2px solid black"}}  onClick={() => navigate(`/edit-post/${props.post.id}`)}>
+            
+
         <h4> {props.post.title} </h4>
         <p> {props.post.content}</p>
     </div>
